7b544452372f3b315eb1a8db4e944a326d30f38d
mdabidind <booksofkid@gmail.com>
2025-06-27 16:09:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'includes/config.php';
+require_once 'includes/functions.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task Manager</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>My Task Manager</h1>
+    <!-- Your HTML/PHP here -->
+</body>
+</html>
