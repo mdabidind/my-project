de1e7a5c08491a755f93d0973574007dc67f42e7
mdabidind <booksofkid@gmail.com>
2025-06-27 15:55:31 +0530

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+// Safe to upload to GitHub (uses placeholders)
+return [
+  'DB_HOST' => 'localhost',
+  'DB_USER' => 'your_username',
+  'DB_PASS' => 'your_password_here', // REPLACE THIS on Hostinger
+  'DB_NAME' => 'task_manager'
+];
