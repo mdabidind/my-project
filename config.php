6d06122dff07346a716d06f43aacb16921fd2ad0
mdabidind <booksofkid@gmail.com>
2025-06-27 16:45:10 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-// config.php (safe for GitHub)
+// includes/config.php (SAFE TO UPLOAD)
 define('DB_HOST', '{{DB_HOST}}');
 define('DB_USER', '{{DB_USER}}');
-define('DB_PASS', '{{DB_PASS}}');
+define('DB_PASS', '{{DB_PASSWORD}}');
+?>
